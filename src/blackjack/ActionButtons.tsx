02a8afc6f0e8a055afedd4eca00df013cff60a7d
jeffrey <jeffreybilodeau5@gmail.com>
2025-02-07 13:35:46 -0500

    Add reactjs-popup for replay confirmation in Blackjack

--- src/blackjack/ActionButtons.tsx
+++ src/blackjack/ActionButtons.tsx
@@ -1,6 +1,8 @@
 import { Button } from "react-bootstrap";
 import EnumWinner from "./EnumWinner";
 import ICarteData from "./ICarteData";
+import Popup from "reactjs-popup";
+import "reactjs-popup/dist/index.css";
 
 interface ActionBoutonProps {
   winner: EnumWinner;
@@ -9,6 +11,7 @@ interface ActionBoutonProps {
   jeuDeCarteId: string;
   cartesJoueur: ICarteData[];
   cartesCroupier: ICarteData[];
+  mise: number;
   setCartesJoueur: (cartes: ICarteData[]) => void;
   setCartesCroupier: (cartes: ICarteData[]) => void;
   setCarteCaché: (carteCaché: boolean) => void;
@@ -16,10 +19,27 @@ interface ActionBoutonProps {
   initPartie: () => void;
   stay: () => void;
   hit: () => void;
-  double: () => void;
+  // double: () => void;
+}
+interface proptest {
+  mise: number;
+  initPartie: () => void;
 }
 
-
+function PopupExample(props: proptest) {
+  return (
+    <Popup trigger={<Button> Replay</Button>} position="top center" mouseLeaveDelay={100} mouseEnterDelay={0} on="hover">
+      <div>
+        <div style={{ display: "flex", justifyContent: "center" }}>Replay with the same bet?</div>
+        <div style={{ display: "flex", justifyContent: "center" }}>Current bet: {props.mise}</div>
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <Button onClick={() => props.initPartie()}>Replay</Button>
+        </div>
+      </div>
+      
+    </Popup>
+  );
+}
 
 export function ActionButtons(props: ActionBoutonProps) {
   return (
@@ -48,7 +68,7 @@ export function ActionButtons(props: ActionBoutonProps) {
         >
           Stay
         </Button>
-        <Button
+        {/* <Button
           variant="warning"
           className={props.winner != EnumWinner.null ? "m-4 disabled" : "m-4 "}
           onClick={async () => {
@@ -56,12 +76,15 @@ export function ActionButtons(props: ActionBoutonProps) {
           }}
         >
           Double
-        </Button>
+        </Button> */}
+        {/* <Button onClick={() => props.initPartie()}>Replay</Button> */}
       </div>
       <div>
-        <Button onClick={() => props.initPartie()}>Reset</Button>
-        
-        <h1>{EnumWinner[props.winner] == 'null' ? '' : EnumWinner[props.winner]}</h1>
+        <PopupExample mise={props.mise} initPartie={props.initPartie} />
+
+        <h1>
+          {EnumWinner[props.winner] == "null" ? "" : EnumWinner[props.winner]}
+        </h1>
       </div>
     </>
   );
